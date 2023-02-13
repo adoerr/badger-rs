4296532de1f4f973cdc1f30471bafe1d3c561fd4
adoerr <0xad@gmx.net>
2023-02-13 08:15:30 +0100

    badger blinky
    Signed-off-by: adoerr <0xad@gmx.net>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::{digital::v2::OutputPin, timer::CountDown};
-use fugit::ExtU32;
-use panic_halt as _;
-use pimoroni_badger2040::{
-    entry, hal,
-    hal::{pac, Timer},
+use bsp::{
+    entry,
+    hal::{
+        clocks::{init_clocks_and_plls, Clock},
+        Sio, Watchdog,
+    },
+    pac,
 };
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::v2::OutputPin;
+use panic_probe as _;
+use pimoroni_badger2040 as bsp;
 
 #[entry]
 fn main() -> ! {
-    // Grab our singleton objects
+    info!("init badger");
+
     let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
 
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
-    let _clocks = hal::clocks::init_clocks_and_plls(
-        pimoroni_badger2040::XOSC_CRYSTAL_FREQ,
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -32,34 +38,25 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    // Set the pins up according to their function on this particular board
-    let pins = pimoroni_badger2040::Pins::new(
+    let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // Configure the timer peripheral to be a CountDown timer for our blinky delay
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
-    let mut delay = timer.count_down();
-
-    // Set the LED to be an output
     let mut led_pin = pins.led.into_push_pull_output();
 
-    // Blink the LED at 1 Hz
     loop {
-        // LED on, and wait for 500ms
+        info!("on!");
         led_pin.set_high().unwrap();
-        delay.start(500.millis());
-        let _ = nb::block!(delay.wait());
+        delay.delay_ms(500);
 
-        // LED off, and wait for 500ms
+        info!("off!");
+
         led_pin.set_low().unwrap();
-        delay.start(500.millis());
-        let _ = nb::block!(delay.wait());
+        delay.delay_ms(500);
     }
 }
