adc6f2a45937778cebaf5b2284c6f6002c5becd2
adoerr <0xad@gmx.net>
2024-04-28 16:19:03 +0200

    task switch
    Signed-off-by: adoerr <0xad@gmx.net>

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,10 @@ mod task;
 
 use bsp::{
     entry,
-    hal::{
-        clocks::{init_clocks_and_plls, Clock},
-        Sio, Watchdog,
-    },
+    hal::{clocks::init_clocks_and_plls, Watchdog},
     pac,
 };
 use defmt::*;
-use embedded_hal::digital::OutputPin;
 use rp_pico as bsp;
 #[allow(unused_imports)]
 use {defmt_rtt as _, panic_probe as _};
@@ -22,13 +18,11 @@ fn main() -> ! {
     info!("init board");
 
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
+    let _ = init_clocks_and_plls(
         external_xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
@@ -40,34 +34,8 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    info!("main stack pointer: {:?}", task::stack_ptr() as usize);
 
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    stack_start();
-
-    info!("current stack pointer: {:?}", task::stack_ptr() as usize);
-
-    let mut led_pin = pins.led.into_push_pull_output();
-
-    loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
-    }
-}
-
-extern "C" {
-    static _stack_start: u8;
-}
-fn stack_start() {
-    let stack_start = unsafe { &_stack_start as *const _ as usize };
-    info!("address of _stack_start: {:?}", stack_start);
+    task::init_scheduler();
+    task::task_switch()
 }
--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,23 @@
-use core::arch::asm;
+use core::{arch::asm, ptr::addr_of_mut};
+
+use cortex_m::asm;
+use defmt::info;
+
+/// Stack size
+pub const STACK_SIZE: usize = 1024;
+
+/// Scheduler task
+pub static mut SCHEDULER: Task = Task {
+    next: None,
+    stack_ptr: 0 as *mut u32,
+    stack: [0; STACK_SIZE],
+};
+
+/// Pointer ot the current task
+static mut CURRENT: *mut Task = 0 as *mut Task;
 
 #[allow(unused)]
+#[repr(C)]
 /// Task structure
 pub struct Task {
     /// next task in the list
@@ -8,7 +25,7 @@ pub struct Task {
     /// saved stack pointer
     pub stack_ptr: *mut u32,
     /// task stack
-    pub stack: [u8; 4096],
+    pub stack: [u8; STACK_SIZE],
 }
 
 /// Return the current stack pointer
@@ -21,4 +38,34 @@ pub fn stack_ptr() -> *mut u32 {
     }
 
     sp
+}
+
+/// Initialize scheduler
+pub fn init_scheduler() {
+    info!("init scheduler with stack pointer: {:?}", stack_ptr());
+
+    unsafe {
+        SCHEDULER.stack_ptr = &mut SCHEDULER.stack[STACK_SIZE - 1] as *mut u8 as *mut u32;
+    }
+}
+
+#[no_mangle]
+fn scheduler() -> ! {
+    info!("call scheduler with stack pointer: {:?}", stack_ptr());
+
+    unsafe {
+        CURRENT = addr_of_mut!(SCHEDULER);
+    }
+    unsafe {
+        info!("current task: {:?}", CURRENT as usize);
+    }
+
+    loop {
+        asm::wfi();
+    }
+}
+
+pub fn task_switch() -> ! {
+    info!("task_switch");
+    scheduler()
 }
