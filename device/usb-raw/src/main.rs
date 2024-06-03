eea8e7c4f3eeb4cf5baaed1bb4fe0d0507fd311f
adoerr <0xad@gmx.net>
2024-06-03 20:39:21 +0200

    set configuration
    Signed-off-by: adoerr <0xad@gmx.net>

--- device/usb-raw/src/main.rs
+++ device/usb-raw/src/main.rs
@@ -27,9 +27,26 @@ async fn main(_spawner: Spawner) {
     // USB driver
     let driver = Driver::new(p.USB, Irqs);
     // USB config
-    let config = Config::new(USB_VENDOR_ID, USB_PRODUCT_ID);
+    let mut cfg = Config::new(USB_VENDOR_ID, USB_PRODUCT_ID);
+    cfg.manufacturer = Some("squirrel");
+    cfg.product = Some("USB squirrel");
+    cfg.serial_number = Some("CAFECAFE");
+    cfg.max_power = 100;
+    cfg.max_packet_size_0 = 64;
+
+    let mut cfg_descriptor = [0u8; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
     // USB device builder
-    let builder = Builder::new(driver, config, &mut [], &mut [], &mut [], &mut []);
+    let builder = Builder::new(
+        driver,
+        cfg,
+        &mut cfg_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
+        &mut control_buf,
+    );
 
     let mut usb = builder.build();
     usb.run().await;
